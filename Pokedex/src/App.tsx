a3e5b59a832eff88e64920ee3aad2ecb99cf2b10
ghuart9207-cmd <ghuart9207@gmail.com>
2026-09-02 16:58:22 +0200

    Feat : création d'un composant (Bulbasaur)

--- Pokedex/src/App.tsx
+++ Pokedex/src/App.tsx
@@ -1,12 +1,11 @@
-import "./App.css";
-
-import MyTitle from "./components/MyTitle";
+import './App.css'
+import PokemonCard from './components/PokemonCard';
 
 function App() {
   return (
-    <section>
-      <MyTitle />
-    </section>
+  <div>
+    <PokemonCard />
+  </div>
   );
 }
 
--- Pokedex/src/components/PokeCard.tsx
+++
@@ -1,5 +0,0 @@
-function PokeCard() {
-  return <h1>Hello React</h1>;
-}
-
-export default PokeCard;
